abf543efc3d37bfa5c06d6b25102f65a6dfd520e
walker.zach <walker.zach@b3a271fa-f289-11dd-8c4e-4bbb75a408b7>
2009-11-21 00:14:24 +0000

    Modified HelloWorld example to use Service class and show asynchronous vs synchronous call structure

--- branches/python_service_devel/python/src/example/helloworld/HelloWorldServiceImpl.py
+++ branches/python_service_devel/python/src/example/helloworld/HelloWorldServiceImpl.py
@@ -28,6 +28,7 @@ May 2009
 '''
 
 import hello_world_pb2
+import time
 
 class HelloWorldImpl(hello_world_pb2.HelloWorldService):
     def HelloWorld(self,controller,request,done):
@@ -42,7 +43,8 @@ class HelloWorldImpl(hello_world_pb2.HelloWorldService):
         # Create a reply
         response = hello_world_pb2.HelloResponse()
         response.hello_world = 'Hello %s' % name
-        
+        #Sleeping to show asynchronous behavior on client end.
+        time.sleep(1)
         # We're done, call the run method of the done callback
         done.run(response)
 
